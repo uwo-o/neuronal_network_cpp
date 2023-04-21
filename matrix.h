c6fc6a70437f62eb5817389c736d03b37293dc7b
uwo-o <hugo.campos@usm.cl>
2023-04-20 20:34:38 -0400

    Commented structures

--- matrix.h
+++ matrix.h
@@ -1,6 +1,12 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+/*
+Matrix struct
+ - int rows: Rows quantity
+ - int cols: Cols quantity
+ - double ** matrix_pointer: Pointer that has the matrix memory direction
+*/
 typedef struct Matrix {
     int rows;
     int cols;
--- neurone.h
+++ neurone.h
@@ -4,10 +4,18 @@
 #include "matrix.h"
 #include "transform.h"
 
+/*
+Neurone struct
+- int childrens_quantity: The children's array size 
+- Neurone * childrens: Pointer that has the Neuone array
+- double value: The neurone's value
+- int layer_id: The layer deeps number
+*/
 typedef struct Neurone {
-    double * parents;
-    double * childrens;
-    double layer;
+    int childrens_quantity;
+    Neurone * childrens;
+    double value;
+    int layer_id;
 } Neurone;
 
 #endif
