d6499163c1389a810ad8cabe2fa8e307594d93b9
uwo-o <hugo.campos@usm.cl>
2023-04-22 20:41:31 -0400

    a bit of functionality implemented

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
+#include <stdio.h>
+
 #include "matrix.h"
 #include "neurone.h"
 #include "transform.h"
 
 int main(int argc, char * argv[]){
+
+    if(argc==1){
+        printf("Please insert a neuron model to load\n");
+    }
+
+    FILE *ptr;
+    ptr = fopen(argv[1], "r");
+
     return 0;
 }
