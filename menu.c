c2148e70217c4fefa652ef51491685a294b4ee2b
uwo-o <hugo.campos@usm.cl>
2023-04-27 13:12:06 -0400

    options base system created, in progress

--- menu.c
+++ menu.c
@@ -1,5 +1,31 @@
 #include "menu.h"
 
-Matrix * laod_model(FILE * ptr){
-    
+Matrix * load_model(FILE * ptr){
+
+}
+
+int options_check(int argc, char * argv){
+
+    for(int i=0; i<argc; i++){
+
+    }
+}
+
+void options (int argc,char * argv){
+
+    int error = options_check(argc, argv);
+
+    if(error){
+        printf("[Syntax Error] Error in option '");
+        printf(argv[error]);
+        printf("doesn't exist. You can use the following options:\n");
+        for(int i=0; i<OPTIONS_QUANTITY; i++){
+            printf(" - ");
+            printf(OPTIONS[OPTIONS_QUANTITY]);
+        }
+    }
+
+    switch(argv[1]){
+        case '-l': load_model(argv[2]); break;
+    }
 }
