0e5b46e3c0f4f2fffb0e86ed1574bd35860e5c39
uwo-o <hugo.campos@usm.cl>
2023-04-27 22:51:20 -0400

    minnor changes

--- menu.h
+++ menu.h
@@ -4,12 +4,13 @@
 #include <stdio.h>
 
 #include "matrix.h"
+#include "network.h"
 
 int OPTIONS_QUANTITY = 2;
 
 char * OPTIONS[] = {'-l', '-h'};
 
-char * OPTIONS_DESCRIPTION[]={
+char * OPTIONS_DESCRIPTION[] = {
     "[load] With this option you can load a model in memory, you must have to insert a model name inside a text file (.txt)",
     "[help] With this option you will get a program description and help, you can insert an option to get a description"
 };
