77f012f6c772a38f246bdd28b7e95ec912fc54a2
uwo-o <hugo.campos@usm.cl>
2023-04-27 22:52:11 -0400

    functions headers defined and libraries imported

--- network.h
+++ network.h
@@ -1,6 +1,9 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "neurone.h"
 
 typedef struct Network{
@@ -11,5 +14,9 @@ typedef struct Network{
     Neurone * input_array;
     Neurone * output_array;
 } Network;
+
+void neurone_connector(Neurone * input_neurones, Neurone * output_neurones, Neurone * hidden_neurones, int input_size, int output_size, int hidden_layers, int hidden_layers_size);
+Network * create_network(int input_size, int output_size, int hidden_layers, int hidden_layers_size);
+Network * load_model(FILE * model);
 
 #endif
