b3c985d2fd6f35ae466604f1f94d9ddb0fedd917
uwo-o <hugo.campos@usm.cl>
2023-05-12 13:14:40 -0400

    load_options code exported to network.cpp

--- main.cpp
+++ main.cpp
@@ -8,23 +8,6 @@
 
 int main(int argc, char * argv[]){
 
-    std::ifstream file("./models/celcius_to_farenheit/model.txt");
-
-    if(!file.is_open()){
-        std::cout << "Error opening file" << std::endl;
-        return 1;
-    }
-
-    int input_size, output_size, hidden_layers, hidden_layers_size;
-
-    file >> input_size;
-    file >> output_size;
-    file >> hidden_layers;
-    file >> hidden_layers_size;
-
-    std::cout << input_size << " " << output_size << " " << hidden_layers << " " << hidden_layers_size << std::endl;
-
-    file.close();
-
+    
     return 0;
 }
