a1dbea7672a038717c0db9a4de2745aa92744fe4
uwo-o <hugo.campos@usm.cl>
2023-05-12 21:23:50 -0400

    options base added

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #include "matrix.hpp"
 #include "neuron.hpp"
 #include "transform.hpp"
+#include "menu.hpp"
+
+using namespace std;
 
 int main(int argc, char * argv[]){
 
+    vector<string> args = vector<string>(argv, argv+argc);
+
+    if(args.size() == 1){
+        cout << "Error: You must insert an option" << endl;
+        return 0;
+    }
+
+    options(argc, args);
     
     return 0;
 }
