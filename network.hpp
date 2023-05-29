239966570e1da6c63e98b94768efd3e1304d0fdc
uwo-o <hugo.campos@usm.cl>
2023-05-29 09:53:10 -0400

    necessary atributes and methos declared for future implementation of connector algorithm

--- network.hpp
+++ network.hpp
@@ -8,6 +8,10 @@
 #include "neuron.hpp"
 
 class Network {
+	private:
+		void connector();
+		int get_next_first_layer_neuron_id(int neuron_id);
+		bool is_final_layer_neyron(int neuron_id);
 	public:
 		Network(int input_size, int output_size, int hidden_layers, int hidden_layers_size);
 		int input_size;
