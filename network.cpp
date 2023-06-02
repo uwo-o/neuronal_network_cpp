a8253571a88cb115adf41d9d7ab5fba13807c892
uwo-o <hugo.campos@usm.cl>
2023-06-01 22:24:50 -0400

    Basic network behavior implemented, no tested and it has errors to solve, its just a first step

--- network.cpp
+++ network.cpp
@@ -14,6 +14,70 @@ Network::Network(int input_size, int output_size, int hidden_layers, int hidden_
 
 	for(int i = 0; i < total_neurons; i++){
 		Neuron *n = new Neuron();
-		nurons.push_back(n);
+		neurons.push_back(n);
 	}
+}
+
+void Network::connector(){
+	stack<int> stack;
+	int current_id=0;
+	for(int i=this->input_size; i>0; i--){
+		stack.push(i-1);
+	}
+	while(!stack.empty()){
+		Neuron * input_neuron = this->neurons[stack.top()];
+		if(input_neuron->status == -1){
+			Link *link = new Link();
+			link->input = input_neuron;
+			input_neuron->status = 0;
+		}
+		int next_neuron_id = get_next_first_layer_neuron_id(stack.top());
+		int layer_offset = next_neuron_id + current_id;
+		if(next_neuron_id >= this->neurons.size() - this->output_size){
+			link->output = this->neurons[layer_offset];
+			this->neurons[layer_offset]->status = 1;
+			current_id+=1;
+		}
+		if(this->neurons[layer_offset]->status == -1){
+			stack.push(layer_offset);
+			link.output = this->neurons[layer_offset];
+			current_id = 0;
+		}else{
+			link.output = this->neurons[layer_offset];
+			current_id+=1;
+		}
+		if(this->is_final_layer_neuron(layer_offset)){
+			stack.pop();
+			input_neuron->status = 1;
+			current_id = 0;
+		}
+	}
+}
+
+int Network::get_next_first_layer_neuron_id(int id){
+	if(id < this->input_size){
+		return this->input_size;
+	}else if(id < this->input_size + this->total_hidden_neurons){
+		return this->input_size + this->hidden_layers_size;
+	}
+	int offset_id = id - this->input_size;
+	float fractional_part = offset_id % 1;
+	float part = (fractional_part/this->hidden_layers) * this->total_hidden_neurons;
+	return (int)part + 1;
+}
+
+bool Network::is_final_layer_neuron(int id){
+	if(id % (this->input_size - 1) == 0){
+		return true;
+	}
+	if(id % (this->neurons.size() - 1) == 0){
+		return true;
+	}
+	for(int i=1; i<=this->hidden_layers; i++){
+		int layer_quantity = this->hidden_layers_size * i;
+		if(id % (layer_quantity - 1) == 0){
+			return true;
+		}
+	}
+	return false;
 }
--- network.hpp
+++ network.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <stack>
 
 #include "neuron.hpp"
 
@@ -11,7 +12,7 @@ class Network {
 	private:
 		void connector();
 		int get_next_first_layer_neuron_id(int neuron_id);
-		bool is_final_layer_neyron(int neuron_id);
+		bool is_final_layer_neuron(int neuron_id);
 	public:
 		Network(int input_size, int output_size, int hidden_layers, int hidden_layers_size);
 		int input_size;
@@ -19,7 +20,7 @@ class Network {
 		int hidden_layers;
 		int hidden_layers_size;
 		static int total_hidden_neurons;
-		std::vector<Neuron *> nurons;
+		std::vector<Neuron *> neurons;
 };
 
 #endif
