c1fa95081623830e2b12506007812201707858cc
uwo-o <hugo.campos@usm.cl>
2023-07-13 18:06:05 -0400

    complety restructured

--- neuron.hpp
+++ neuron.hpp
@@ -2,32 +2,32 @@
 #define NEURON_HPP
 
 #include <vector>
+#include <tuple>
+#include <stdlib.h>
 
 class Neuron {
 	private:
-		double value, bias;
+		double value;
 		static int global_id;
 		const int id;
 		int status;
 	protected:
 		std::vector<Neuron *> parents_neurons;
-		std::vector<Neuron *> children_neurons;
+		std::vector<std::tuple<double, Neuron *>> children_neurons;
 	public:
 
 		// The constructor creates every neuron with random initial values, these values will be changed during the learning process
-		Neuron() : id(global_id++), value((double)rand()), bias((double)rand()), status(-1) {};	
-		
+		Neuron() : id(global_id++), value((double)rand()), status(-1) {};	
+
 		void synapse();
 
 		double apply_activation_function(double x);
 
-		double get_bias(){return bias;};
-		double get_value(){return value;};
-		int get_status(){return status;};
-		int get_id(){return id;};
+		double get_value(){return this->value;};
+		int get_status(){return this->status;};
+		int get_id(){return this->id;};
 
 		void set_value(double value){this->value = value;};
-		void set_bias(double bias){this->bias = bias;};
 		void set_status(int status){this->status = status;};
 };
 
