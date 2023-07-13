b6b8dc7f007ce85cae9e2b78f9d1f6a469dafa7d
uwo-o <hugo.campos@usm.cl>
2023-07-13 18:05:37 -0400

    complety restructured

--- network.cpp
+++ network.cpp
@@ -1,7 +1,5 @@
 #include "network.hpp"
 
-using namespace std;
-
 // Network constructor
 Network::Network(int input_size, int output_size, int hidden_layers, int hidden_layers_size){
 	this->input_size = input_size;
@@ -12,72 +10,72 @@ Network::Network(int input_size, int output_size, int hidden_layers, int hidden_
 
 	int total_neurons = input_size + output_size + hidden_layers * hidden_layers_size;
 
-	for(int i = 0; i < total_neurons; i++){
-		Neuron *n = new Neuron();
-		neurons.push_back(n);
+	this->input_neurons = new std::vector<Neuron *>();
+	this->hidden_neurons = new std::vector<std::vector<Neuron *> *>();
+	this->output_neurons = new std::vector<Neuron *>();
+
+	for(int i=0; i<this->input_size; i++){
+		Neuron * neuron = new Neuron();
+		this->input_neurons->push_back(neuron);
+	}
+
+	std::vector<Neuron *> * layer = new std::vector<Neuron *>();
+	for(int i=0; i<this->total_hidden_neurons; i++){
+		if(i % this->hidden_layers_size == 0){
+			this->hidden_neurons->push_back(layer);
+			layer = new std::vector<Neuron *>();
+		}
+		layer->push_back(new Neuron());
+	}
+
+	for(int i=0; i<this->output_size; i++){
+		Neuron * neuron = new Neuron();
+		this->output_neurons->push_back(neuron);
+	}
+
+	std::stack<Neuron *> * connector_stack = new std::stack<Neuron *>();
+	this->connector(connector_stack, this->input_neurons->front(), 0);
+}
+
+Neuron * Network::get_unconnected_neuron(int layer){
+	layer--;
+	for(int i=0; i<this->hidden_layers_size; i++){
+		if(this->hidden_neurons[layer][i]->get_status() == -1){
+			return this->hidden_neurons[layer][i];
+		}
+	}
+	return NULL;
+}
+
+int Network::get_layer(int neuron_id){
+	// use math formulas to return the layer
+	if (neuron_id < this->input_size){
+		return 0;
+	}
+	else if (neuron_id < this->input_size + this->total_hidden_neurons){
+		return 1 + (neuron_id - this->input_size) / this->hidden_layers_size;
+	}
+	else{
+		return this->hidden_layers + 1;
 	}
 }
 
-void Network::connector(){
-	stack<int> stack;
-	int current_id=0;
-	for(int i=this->input_size; i>0; i--){
-		stack.push(i-1);
+Network::connector(std::stack <Neuron *> * stack, Neuron * current, int layer){
+	if(current->get_status == -1){
+		current->set_status(0);
+		stack->push(current);
 	}
-	while(!stack.empty()){
-		Neuron * input_neuron = this->neurons[stack.top()];
-		if(input_neuron->status == -1){
-			Link *link = new Link();
-			link->input = input_neuron;
-			input_neuron->status = 0;
-		}
-		int next_neuron_id = get_next_first_layer_neuron_id(stack.top());
-		int layer_offset = next_neuron_id + current_id;
-		if(next_neuron_id >= this->neurons.size() - this->output_size){
-			link->output = this->neurons[layer_offset];
-			this->neurons[layer_offset]->status = 1;
-			current_id+=1;
-		}
-		if(this->neurons[layer_offset]->status == -1){
-			stack.push(layer_offset);
-			link.output = this->neurons[layer_offset];
-			current_id = 0;
-		}else{
-			link.output = this->neurons[layer_offset];
-			current_id+=1;
-		}
-		if(this->is_final_layer_neuron(layer_offset)){
-			stack.pop();
-			input_neuron->status = 1;
-			current_id = 0;
+	Neuron * next = this->get_unconnected_neuron(layer+1);
+	if(next != NULL){
+		current->children_neurons->push_back(std::make_tuple((double)rand(), next));
+		next->parents_neurons->push_back(current);
+		Network->connector(stack, next, layer+1);
+	}
+	else{
+		current->set_status(1);
+		stack->pop();
+		if(!stack->empty()){
+			Network->connector(stack, stack->top(), layer-1);
 		}
 	}
-}
-
-int Network::get_next_first_layer_neuron_id(int id){
-	if(id < this->input_size){
-		return this->input_size;
-	}else if(id < this->input_size + this->total_hidden_neurons){
-		return this->input_size + this->hidden_layers_size;
-	}
-	int offset_id = id - this->input_size;
-	float fractional_part = offset_id % 1;
-	float part = (fractional_part/this->hidden_layers) * this->total_hidden_neurons;
-	return (int)part + 1;
-}
-
-bool Network::is_final_layer_neuron(int id){
-	if(id % (this->input_size - 1) == 0){
-		return true;
-	}
-	if(id % (this->neurons.size() - 1) == 0){
-		return true;
-	}
-	for(int i=1; i<=this->hidden_layers; i++){
-		int layer_quantity = this->hidden_layers_size * i;
-		if(id % (layer_quantity - 1) == 0){
-			return true;
-		}
-	}
-	return false;
 }
