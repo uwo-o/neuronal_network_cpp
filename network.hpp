05e544d8f2571481239088daef9f861dc9f27a6f
uwo-o <hugo.campos@usm.cl>
2023-07-13 18:05:46 -0400

    restructured

--- network.hpp
+++ network.hpp
@@ -10,17 +10,25 @@
 
 class Network {
 	private:
-		void connector();
-		int get_next_first_layer_neuron_id(int neuron_id);
-		bool is_final_layer_neuron(int neuron_id);
-	public:
-		Network(int input_size, int output_size, int hidden_layers, int hidden_layers_size);
+		void connector(std::stack <Neuron *> * stack, Neuron * current, int layer);
+		Neuron * get_unconnected_neuron(int layer);
+		int get_layer(int neuron_id);
 		int input_size;
 		int output_size;
 		int hidden_layers;
 		int hidden_layers_size;
 		static int total_hidden_neurons;
-		std::vector<Neuron *> neurons;
+		std::vector<Neuron *> * input_neurons;
+		std::vector<std::vector <Neuron *> *> * hidden_neurons; 
+		std::vector<Neuron *> * output_neurons;
+	public:
+		Network(int input_size, int output_size, int hidden_layers, int hidden_layers_size);
+
+		int get_input_size(){return this->input_size;};
+		int get_output_size(){return this->output_size;};
+		int get_hidden_layers(){return this->hidden_layers;};
+		int get_hidden_layers_size(){return this->hidden_layers_size;};
+		int get_total_hidden_neurons(){return this->total_hidden_neurons;};
 };
 
 #endif
