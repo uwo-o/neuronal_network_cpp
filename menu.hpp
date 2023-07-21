b5b30a8d37e95e1f9ff3e4afe458b600ab3d5934
uwo-o <hugo.campos@usm.cl>
2023-07-21 15:19:32 -0400

    new functions to implement

--- menu.hpp
+++ menu.hpp
@@ -1,15 +1,7 @@
 #ifndef MENU_HPP
 #define MENU_HPP
 
-#include <array>
-#include <vector>
-#include <tuple>
-#include <string>
-#include <iostream>
-
-using namespace std;
-
-bool options_check(int argc, vector<string> args);
-void options (int argc, vector<string> args);
+void no_model();
+void model_loaded();
 
 #endif
