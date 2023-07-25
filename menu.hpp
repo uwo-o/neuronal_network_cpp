1332dc2c35be606e1bc81af6829259f7eb9723a4
uwo-o <hugo.campos@usm.cl>
2023-07-25 02:59:08 -0400

    menu class declared

--- menu.hpp
+++ menu.hpp
@@ -1,7 +1,23 @@
 #ifndef MENU_HPP
 #define MENU_HPP
 
-void no_model();
-void model_loaded();
+#include <iostream>
+
+#include "network.hpp"
+
+class Menu{
+    private:
+        int model_loaded;
+        char *model_name;
+        Network *network;
+    public:
+        Menu();
+        void print_menu();
+        void load_model();
+        void print_status();
+        void show();
+        bool update_state(int);
+        int selection();
+};
 
 #endif
