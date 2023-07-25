f06993585447144da7fdf1b19bbb5ad2fae620b7
uwo-o <hugo.campos@usm.cl>
2023-07-25 03:06:23 -0400

    not used includes removed

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-
-#include "neuron.hpp"
-#include "network.hpp"
 #include "menu.hpp"
-#include "constants.hpp"
 
 int main(int argc, char * argv[]){
 
