2c8a3bac8f202e044747f56cf9cd69e4a9dd5e50
uwo-o <hugo.campos@usm.cl>
2024-05-26 19:10:37 -0400

    names and logic change

---
+++ includes/layer.h
@@ -0,0 +1,28 @@
+#ifndef LAYER_H
+#define LAYER_H
+
+#include <iostream>
+
+#include "math/matrix.h"
+#include "math/vector.h"
+
+class Layer {
+    private:
+        int id;
+        Vector *neurons;
+        Vector *input;
+        Matrix *weights;
+        Matrix *biases;
+
+    public:
+        Layer(int id);
+        Layer(int id, int neurons, int input_size);
+        ~Layer();
+
+        Layer * copy();
+
+        void print();
+
+        void set_input(Vector *input);
+        Vector * get_output();
+};
