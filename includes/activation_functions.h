d35fee6ad3aa36bbff55757026155e643f1305f7
uwo-o <hugo.campos@usm.cl>
2024-05-26 23:18:33 -0400

    activations functions derivated

--- includes/activation_functions.h
+++ includes/activation_functions.h
@@ -4,6 +4,9 @@
 #include <cmath>
 
 double ReLU(double x){return x > 0 ? x : 0;};
+double dReLU(double x){return x > 0 ? 1 : 0;};
+
 double sigmoide(double x){return 1/(1+exp(-x));};
+double dsigmoide(double x){return sigmoide(x)*(1-sigmoide(x));};
 
 #endif
