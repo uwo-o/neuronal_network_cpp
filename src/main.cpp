e53b39014c4547e228474822fd6743a3e2cedb02
uwo-o <hugo.campos@usm.cl>
2024-07-15 22:01:31 -0400

    working on a new implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,72 @@
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 
 #include "network.h"
+#include <unistd.h>
 
 using namespace std;
 
-static const int INPUT_SIZE = 2;
-static const int HIDDEN_SIZE = 3;
+static const int INPUT_SIZE = 1;
+static const int HIDDEN_SIZE = 0;
+static const int HIDDEN_LAYERS = 0;
 static const int OUTPUT_SIZE = 1;
 
-int main() {
-    
-    Network network(INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE);
-    network.describe();
+static const int TRAIN_SIZE = 10;
+static const float LEARN_RATE = 0.00001;
+
+float train[TRAIN_SIZE][2] = {
+    {0, 32},
+    {10, 50},
+    {30, 86},
+    {50, 122},
+    {100, 212},
+    {22, 76.1},
+    {56, 132.8},
+    {89, 192.2},
+    {54, 129.2},
+    {5, 41}};
+
+float rand_f()
+{
+    srand(time(0));
+    return (float)rand() / RAND_MAX;
+}
+
+float cost(float val, float exp)
+{
+    float diff = val - exp;
+    float sq = diff * diff;
+    return sq;
+}
+float error_d(float val, float exp)
+{
+    return (val - exp) * 2;
+}
+
+int main()
+{
+
+    float w = rand_f() * 10;
+    // wait one second
+    sleep(1);
+
+    float b = rand_f() * 10;
+
+    for (size_t i = 0; i < 100000; ++i)
+    {
+        for (size_t j = 0; j < TRAIN_SIZE; ++j)
+        {
+            float x = train[j][0];
+            float y = w * x + b;
+            float exp_x = train[j][1];
+            float dx = error_d(y, exp_x);
+            w -= LEARN_RATE * dx * x;
+            b -= LEARN_RATE * dx;
+
+            cout << "W: " << w << " B: " << b << endl;
+        }
+    }
 
     return 0;
 }
--- src/network.cpp
+++ src/network.cpp
@@ -1,21 +1,27 @@
 #include "network.h"
 
-Network::Network(int input_size, int hidden_size, int output_size) {
-    this->input = Eigen::MatrixXd(input_size, 1);
+Network::Network(int input_size, int hidden_size, int hidden_layers, int output_size)
+{
+    this->input = Eigen::MatrixXd(input_size, hidden_size);
     this->input.setRandom();
 
-    this->hidden = Eigen::MatrixXd(hidden_size, input_size);
+    this->hidden = Eigen::MatrixXd(hidden_size, hidden_layers);
     this->hidden.setRandom();
 
-    this->output = Eigen::MatrixXd(output_size, hidden_size);
+    this->output = Eigen::MatrixXd(hidden_layers, output_size);
     this->output.setRandom();
 }
 
-Network::~Network() {
+Network::~Network()
+{
 }
 
-void Network::describe() {
-    std::cout << "Input:\n" << this->input << std::endl;
-    std::cout << "Hidden:\n" << this->hidden << std::endl;
-    std::cout << "Output:\n" << this->output << std::endl;
+void Network::describe()
+{
+    std::cout << "Input:\n"
+              << this->input << std::endl;
+    std::cout << "Hidden:\n"
+              << this->hidden << std::endl;
+    std::cout << "Output:\n"
+              << this->output << std::endl;
 }
